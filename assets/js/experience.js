a9c9014e6baf79acf61868f3be2eb551a28ff4d5
Rebekah Davies <rebekah@backbase.com>
2019-11-10 08:43:53 +0000

    remove document.ready function

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,26 +1,24 @@
-$(document).ready(function(){
-    let modal;
-    //load navigation
-    $('#nav').load("templates/nav.html");
+let modal;
+//load navigation
+$('#nav').load("templates/nav.html");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks the button, open the modal 
-    function openModel(modelId){
-        modal = document.getElementById(modelId)
-        model.style.display = "block";
-    }
+// When the user clicks the button, open the modal 
+function openModel(modelId){
+    modal = document.getElementById(modelId)
+    model.style.display = "block";
+}
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
     modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    }
- });
+}
+}
