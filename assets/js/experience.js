7aa53e858ccd29037ad8a79ca7e2ca26b31916f8
Rebekah Davies <rebekah@backbase.com>
2019-11-10 08:19:33 +0000

    change functions to html on click

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
     //load navigation
     $('#nav').load("templates/nav.html");
-    
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-    modal.style.display = "block";
+    function openModel(model){
+        el = document.getElementById(model);
+        el.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
